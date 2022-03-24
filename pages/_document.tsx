8cd3fec23fb0aa613e76b122b7c987227fee296d
barman47 <nomsouzoanya@yahoo.co.uk>
2022-03-24 14:58:19 +0100

    Fixed Loading ServerStylesheets before page render

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@mui/styles';
 
 class MyDocument extends Document {
 	render() {
@@ -20,5 +22,21 @@ class MyDocument extends Document {
 		);
 	}
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+	const sheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
+
+	ctx.renderPage = () => originalRenderPage({
+		enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+	});
+
+	const initialProps = await Document.getInitialProps(ctx);
+
+	return {
+		...initialProps,
+		styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+	};
+};
 
 export default MyDocument;
